7324e882e9a1a3ad573a514c5b20cdb29d818625
heshenghuan <heshenghuan@sina.com>
2018-07-22 18:29:25 +0800

    course 18.1

---
+++ 18.1/js/main.js
@@ -0,0 +1,14 @@
+/******************************************
+ *  Author : Heshenghuan   
+ *  Created On : Sun Jul 22 2018
+ *  File : main.js
+ *******************************************/
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        name: 'Anna',
+        age: 18,
+        gender: 'Female'
+    }
+});
